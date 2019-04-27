aeebd73a185a3ee6f1c2c0b5e26d481225279b4d
Marwa Alqatari <malqata2@uncc.edu>
2019-04-27 23:04:05 +0000

    Commit uncommitted changes before pulling sign in dev

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_27_172412) do
+ActiveRecord::Schema.define(version: 2019_04_27_225926) do
 
   create_table "buddies", force: :cascade do |t|
     t.string "fname"
@@ -38,13 +38,12 @@ ActiveRecord::Schema.define(version: 2019_04_27_172412) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "encrypted_password", limit: 128
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
-    t.string "remember_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
