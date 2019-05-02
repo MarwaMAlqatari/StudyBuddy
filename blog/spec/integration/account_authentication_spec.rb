73ed62346e955ede86b0722184f0fbf719bc3282
Marwa Alqatari <malqata2@uncc.edu>
2019-05-02 19:35:39 +0000

    finished 5 testing scenarios

--- blog/spec/integration/account_authentication_spec.rb
+++ blog/spec/integration/account_authentication_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper.rb'
 
 feature "Account authentication" do
-    scenario "Account sign in" do 
-        
-        
+    
+    
+    scenario "User successfully signs up for the website" do
+            visit root_path
+            expect(page).to have_content("Sign Up")
+            click_button "Sign Up"
+            expect(page).to have_field("email")
+            expect(page).to have_field("password")
+             fill_in "email", with: "marwais@awesome.com"
+             fill_in "password", with: "1234"
     end
-    scenario "Account sign up" do
-        
-        
+    scenario "User successfully signs in for the website" do 
+            visit root_path
+            expect(page).to have_content("Sign In")
+            click_button "Sign In"
+            expect(page).to have_field("email")
+            expect(page).to have_field("password")
+             fill_in "email", with: "marwais@awesome.com"
+              fill_in "email", with: "1234"
     end
     
 
